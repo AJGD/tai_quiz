dc14559908f143d3bd5a21a68b35998a9b8c2a83
AJGD <alexanddra@op.pl>
2019-04-27 19:43:51 +0200

    [CREATE] redirect to quiz view after creating, customize quiz page if logged user is the author

--- quiz/quizzes/views.py
+++ quiz/quizzes/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpRequest
-from django.contrib import messages
 import requests
+from django.contrib import messages
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, redirect
 
-from .models import Player, Quiz
 from .forms import CreateQuizForm, CreateQuestionForm
+from .models import Player, Quiz
 
 
 def index(_request: HttpRequest) -> HttpResponse:
@@ -32,12 +32,16 @@ def create_quiz(request: HttpRequest) -> HttpResponse:
         form = CreateQuizForm(request.POST)
         if form.is_valid():
             added_quiz = form.save(commit=False)
-            # mock_author = Player(username="Kleofas", email="kleo@example.com")
+            # mock_author = Player(username="w3d", email="kleo@example.com")
             # mock_author.save()
-            added_quiz.author = Player.objects.get(username="Kleofas")
-            added_quiz.save()
-            messages.success(request, 'Quiz successfully added!')
-            return redirect('quizzes:create_question')
+            if request.user.is_authenticated:
+                added_quiz.author = Player.objects.get(pk=request.user.id)
+                added_quiz.save()
+                messages.success(request, 'Quiz successfully added!')
+                return redirect('quizzes:quiz', quiz_id=added_quiz.id)
+            else:
+                messages.error(request, 'unable to add quiz when user is not logged in')
+                return redirect('quizzes:index')
         else:
             return HttpResponse("WHAT ARE YOU DOING?")
     else:
@@ -49,7 +53,7 @@ def create_quiz(request: HttpRequest) -> HttpResponse:
 
 
 def create_question(request: HttpRequest) -> HttpResponse:
-    """Render the create quiz page"""
+    """Render the create question page"""
     if request.method == 'POST':
         form = CreateQuestionForm(request.POST)
         if form.is_valid():
@@ -95,4 +99,6 @@ def choose_quiz_to_play(request: HttpRequest) -> HttpResponse:
 
 def start_quiz(request, quiz_id):
     """Render the single quiz page"""
+    if request.user.is_authenticated and (Quiz.objects.get(pk=quiz_id).author.id == request.user.id):
+        return render(request, 'quiz_edit_page.html', context={'id': quiz_id})
     return render(request, 'quiz_start_page.html', context={'id': quiz_id})
