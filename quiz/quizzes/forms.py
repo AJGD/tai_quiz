24d81ba3b2d02548084179bd909af1f22906c2c9
Magdalena Cudak <magdalena.cudak@gmail.com>
2019-05-12 17:43:23 +0200

    Add form to hide words

--- quiz/quizzes/forms.py
+++ quiz/quizzes/forms.py
@@ -63,3 +63,7 @@ class ChooseArticleForm(forms.Form):
     def __init__(self, articles, *args, **kwargs):
         super(ChooseArticleForm, self).__init__(*args, **kwargs)
         self.fields['Choose article'] = forms.ChoiceField(choices=articles, widget=forms.RadioSelect)
+
+
+class ChooseWordToHide(forms.Form):
+    word = forms.CharField(max_length=200)
