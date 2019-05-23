fd5e30ea763b46eb1c74c5e8d8dd0790eef773dc
AJGD <alexanddra@op.pl>
2019-05-23 21:29:29 +0200

    privacy policy

--- quiz/accounts/views.py
+++ quiz/accounts/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -15,3 +17,7 @@ class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+def show_privacy_policy(request: HttpRequest) -> HttpResponse:
+    return render(request, 'privacy_policy.html')
