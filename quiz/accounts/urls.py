f530ff2eba60d2c037b19ffeecd4806887ec3065
AJGD <alexanddra@op.pl>
2019-05-23 21:29:29 +0200

    privacy policy

--- quiz/accounts/urls.py
+++ quiz/accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('privacy_policy/', views.show_privacy_policy, name='privacy_policy')
 ]
